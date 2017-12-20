967b9dbdc442bf99d37e19b072935e2bd20abf1a
DESKTOP-J19U3H2\Alexis <alexiscalingasan@gmail.com>
2017-12-21 05:33:33 +0800

    initial commit

---
+++ ExcelReader/ExcelReader/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace ExcelReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.resultGrid = new MetroFramework.Controls.MetroGrid();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnBrowse = new MetroFramework.Controls.MetroButton();
+            this.txtPath = new MetroFramework.Controls.MetroTextBox();
+            this.metroPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.resultGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.Controls.Add(this.resultGrid);
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(23, 169);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(690, 188);
+            this.metroPanel1.TabIndex = 0;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            // 
+            // resultGrid
+            // 
+            this.resultGrid.AllowUserToResizeRows = false;
+            this.resultGrid.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.resultGrid.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.resultGrid.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.resultGrid.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.resultGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.resultGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.resultGrid.DefaultCellStyle = dataGridViewCellStyle5;
+            this.resultGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.resultGrid.EnableHeadersVisualStyles = false;
+            this.resultGrid.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.resultGrid.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.resultGrid.Location = new System.Drawing.Point(0, 0);
+            this.resultGrid.Name = "resultGrid";
+            this.resultGrid.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.resultGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.resultGrid.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.resultGrid.RowTemplate.Height = 28;
+            this.resultGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.resultGrid.Size = new System.Drawing.Size(690, 188);
+            this.resultGrid.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.btnBrowse.FontWeight = MetroFramework.MetroButtonWeight.Regular;
+            this.btnBrowse.Location = new System.Drawing.Point(598, 115);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(115, 40);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.btnBrowse.UseSelectable = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtPath
+            // 
+            // 
+            // 
+            // 
+            this.txtPath.CustomButton.Image = null;
+            this.txtPath.CustomButton.Location = new System.Drawing.Point(531, 2);
+            this.txtPath.CustomButton.Name = "";
+            this.txtPath.CustomButton.Size = new System.Drawing.Size(35, 35);
+            this.txtPath.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtPath.CustomButton.TabIndex = 1;
+            this.txtPath.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtPath.CustomButton.UseSelectable = true;
+            this.txtPath.CustomButton.Visible = false;
+            this.txtPath.FontSize = MetroFramework.MetroTextBoxSize.Medium;
+            this.txtPath.Lines = new string[0];
+            this.txtPath.Location = new System.Drawing.Point(23, 115);
+            this.txtPath.MaxLength = 32767;
+            this.txtPath.Name = "txtPath";
+            this.txtPath.PasswordChar = '\0';
+            this.txtPath.ReadOnly = true;
+            this.txtPath.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtPath.SelectedText = "";
+            this.txtPath.SelectionLength = 0;
+            this.txtPath.SelectionStart = 0;
+            this.txtPath.ShortcutsEnabled = true;
+            this.txtPath.Size = new System.Drawing.Size(569, 40);
+            this.txtPath.TabIndex = 2;
+            this.txtPath.UseSelectable = true;
+            this.txtPath.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtPath.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 464);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.metroPanel1);
+            this.Name = "Form1";
+            this.Text = "Excel Reader";
+            this.metroPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.resultGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private MetroFramework.Controls.MetroButton btnBrowse;
+        private MetroFramework.Controls.MetroTextBox txtPath;
+        private MetroFramework.Controls.MetroGrid resultGrid;
+    }
+}
+
---
+++ ExcelReader/ExcelReader/Form1.cs
@@ -0,0 +1,50 @@
+﻿using ExcelDataReader;
+using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ExcelReader
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            this.openFileDialog1.Filter = "Excel Files(.xlsx)|*.xlsx";
+            this.openFileDialog1.Title = "Select an excel file";
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.txtPath.Text = openFileDialog1.FileName;
+                FileStream stream = File.Open(openFileDialog1.FileName, FileMode.Open, FileAccess.Read);
+                IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);                
+                DataSet result = excelReader.AsDataSet();
+
+
+                var people = new List<Person>();
+                while (excelReader.Read())
+                {
+                    people.Add(new Person
+                    {
+                        FirstName = excelReader.GetString(0),
+                        LastName = excelReader.GetString(1)
+                    });
+                }
+
+                this.resultGrid.DataSource = people;
+            }
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
